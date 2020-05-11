8286ea89975b01c2814b102ba73e1ce38a9529f9
VladimirGlinskikh <support@zhelezyaka.kz>
2020-05-11 21:09:02 +0600

    pattern Singleton

---
+++ src/main/java/patterns/Singleton.java
@@ -0,0 +1,19 @@
+package patterns;
+
+public class Singleton {
+    private static Singleton singleton;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if (singleton == null) {
+            synchronized (Singleton.class) {
+                if (singleton == null) {
+                    singleton = new Singleton();
+                }
+            }
+        }
+        return singleton;
+    }
+}
