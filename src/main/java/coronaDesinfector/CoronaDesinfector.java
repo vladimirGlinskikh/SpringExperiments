32331db469508bb729c669d592c39f0321870d51
VladimirGlinskikh <support@zhelezyaka.kz>
2020-05-11 20:55:29 +0600

    so it is begin project

---
+++ src/main/java/coronaDesinfector/CoronaDesinfector.java
@@ -0,0 +1,11 @@
+package coronaDesinfector;
+
+public class CoronaDesinfector {
+    public void start(Room room) {
+        desinfect(room);
+    }
+
+    private void desinfect(Room room) {
+        System.out.println("Corona virus coming away now!");
+    }
+}
---
+++ src/main/java/coronaDesinfector/Room.java
@@ -0,0 +1,4 @@
+package coronaDesinfector;
+
+public class Room {
+}
