cf9007c12d17cb5016ebedc79ec5ee0d8236df0d
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-01-09 22:23:20 +0600

    Implementation of simple values

---
+++ src/main/java/implementationOfSimpleValues/InjectSimple.java
@@ -0,0 +1,52 @@
+package implementationOfSimpleValues;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class InjectSimple {
+    private String name;
+    private int age;
+    private float height;
+    private boolean programmer;
+    private Long ageInSeconds;
+
+    public static void main(String[] args) {
+        GenericXmlApplicationContext context = new GenericXmlApplicationContext();
+        context.load("app-context-xml.xml");
+        context.refresh();
+
+        InjectSimple simple = (InjectSimple) context.getBean("injectSimple");
+        System.out.println(simple);
+        context.close();
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public void setProgrammer(boolean programmer) {
+        this.programmer = programmer;
+    }
+
+    public void setAgeInSeconds(Long ageInSeconds) {
+        this.ageInSeconds = ageInSeconds;
+    }
+
+    @Override
+    public String toString() {
+        return "InjectSimple{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", height=" + height +
+                ", programmer=" + programmer +
+                ", ageInSeconds=" + ageInSeconds +
+                '}';
+    }
+}
